ccebb8f8d5e8a392a1202a4a82e526014101d784
Chihab Eddine <ga_benamara@esi.dz>
2020-12-28 16:24:16 +0100

    [KNP] start naive solution

---
+++ k_nearst_points/k_nearst_naive.cpp
@@ -0,0 +1,7 @@
+/* Description :  This document solves the k-nearst points to the origin */
+/*----------------------------------------------------------------------*/
+/*  Problem :  
+             input  : ([(x,y)],k) list of points in 2D plan and an integer k
+             output : [(x,y)] a list of size k for the nearest to origin 
+*/
+/*----------------------------------------------------------------------*/
